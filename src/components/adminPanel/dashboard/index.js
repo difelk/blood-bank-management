77fa427402219068a7c7bb6e9ba69e1f33c050ca
difelk <dilshan.f@theavo.net>
2024-02-21 17:26:01 +0530

    admin panel changes v2

--- src/components/adminPanel/dashboard/index.js
+++ src/components/adminPanel/dashboard/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Styles from "./Dashboard.module.scss"; 
+import React from "react";
+import Styles from "./Dashboard.module.scss";
+import SideBar from "./SideBar";
+import Content from "./Content";
+import Schedules from "./Schedules";
 
 const Dashboard = () => {
-
-
   return (
-    <div className={Styles.header}>
-        <h1>hi admin</h1>
+    <div className={Styles.dashboardWrapper}>
+      <div className={Styles.sectionSideBar}>
+        <SideBar />
+      </div>
+      <div className={Styles.sectionContent}>
+        <Content />
+      </div>
+      <div className={Styles.sectionSchedule}>
+        <Schedules />
+      </div>
     </div>
   );
 };
