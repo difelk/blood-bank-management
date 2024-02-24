87a64c8433602dc3a26784c1f27427e85a24ae37
difelk <dilshan.f@theavo.net>
2024-02-24 06:40:26 +0530

    created custom dropdown for top navigation. dashboard changes v09

--- src/components/adminPanel/dashboard/SideBar.js
+++ src/components/adminPanel/dashboard/SideBar.js
@@ -20,146 +20,158 @@ const SideBar = ({ setCurrentPage }) => {
   };
 
   return (
-    <div className={Styles.sidebarContainer}>
-      <div className={Styles.sidebarWrapper}>
-        <div className={Styles.logowrapper}>
-          <img src={siteLogo} alt="logo" />
-          <p>BloodCentral Network</p>
-        </div>
-        {/* <div>
+    <>
+      {isModalOpen ? (
+        <CustomModal open={setIsModalOpen} title={"Logout"} />
+      ) : (
+        ""
+      )}
+      <div className={Styles.sidebarContainer}>
+        <div className={Styles.sidebarWrapper}>
+          <div className={Styles.logowrapper}>
+            <img src={siteLogo} alt="logo" />
+            <p>BloodCentral Network</p>
+          </div>
+          {/* <div>
         <button>Registor</button>
       </div> */}
-        <div className={Styles.navlinksWrapper}>
-          <ul>
-            <CustomButton
-              buttonType={
-                activeTab === TAB.DASHBOARD
-                  ? BUTTONTYPES.WHITE
-                  : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.DASHBOARD}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={activeTab === TAB.DASHBOARD ? "#5c5dce" : "#a6a6ed"}
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.DASHBOARD ? true : false}
-              onClick={() => getSelectedNavigationOption(TAB.DASHBOARD)}
-            />
+          <div className={Styles.navlinksWrapper}>
+            <ul>
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.DASHBOARD
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.DASHBOARD}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={activeTab === TAB.DASHBOARD ? "#5c5dce" : "#a6a6ed"}
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.DASHBOARD ? true : false}
+                onClick={() => getSelectedNavigationOption(TAB.DASHBOARD)}
+              />
 
-            <CustomButton
-              buttonType={
-                activeTab === TAB.STOCK_MANAGEMENT
-                  ? BUTTONTYPES.WHITE
-                  : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.STOCK_MANAGEMENT}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={
-                    activeTab === TAB.STOCK_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
-                  }
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.STOCK_MANAGEMENT ? true : false}
-              onClick={() => getSelectedNavigationOption(TAB.STOCK_MANAGEMENT)}
-            />
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.STOCK_MANAGEMENT
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.STOCK_MANAGEMENT}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={
+                      activeTab === TAB.STOCK_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
+                    }
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.STOCK_MANAGEMENT ? true : false}
+                onClick={() =>
+                  getSelectedNavigationOption(TAB.STOCK_MANAGEMENT)
+                }
+              />
 
-            <CustomButton
-              buttonType={
-                activeTab === TAB.HOSPITAL_MANAGEMENT
-                  ? BUTTONTYPES.WHITE
-                  : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.HOSPITAL_MANAGEMENT}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={
-                    activeTab === TAB.HOSPITAL_MANAGEMENT
-                      ? "#5c5dce"
-                      : "#a6a6ed"
-                  }
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.HOSPITAL_MANAGEMENT ? true : false}
-              onClick={() =>
-                getSelectedNavigationOption(TAB.HOSPITAL_MANAGEMENT)
-              }
-            />
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.HOSPITAL_MANAGEMENT
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.HOSPITAL_MANAGEMENT}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={
+                      activeTab === TAB.HOSPITAL_MANAGEMENT
+                        ? "#5c5dce"
+                        : "#a6a6ed"
+                    }
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.HOSPITAL_MANAGEMENT ? true : false}
+                onClick={() =>
+                  getSelectedNavigationOption(TAB.HOSPITAL_MANAGEMENT)
+                }
+              />
 
-            <CustomButton
-              buttonType={
-                activeTab === TAB.DONOR_MANAGEMENT
-                  ? BUTTONTYPES.WHITE
-                  : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.DONOR_MANAGEMENT}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={
-                    activeTab === TAB.DONOR_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
-                  }
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.DONOR_MANAGEMENT ? true : false}
-              onClick={() => getSelectedNavigationOption(TAB.DONOR_MANAGEMENT)}
-            />
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.DONOR_MANAGEMENT
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.DONOR_MANAGEMENT}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={
+                      activeTab === TAB.DONOR_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
+                    }
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.DONOR_MANAGEMENT ? true : false}
+                onClick={() =>
+                  getSelectedNavigationOption(TAB.DONOR_MANAGEMENT)
+                }
+              />
 
-            <CustomButton
-              buttonType={
-                activeTab === TAB.EVENT_MANAGEMENT
-                  ? BUTTONTYPES.WHITE
-                  : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.EVENT_MANAGEMENT}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={
-                    activeTab === TAB.EVENT_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
-                  }
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.EVENT_MANAGEMENT ? true : false}
-              onClick={() => getSelectedNavigationOption(TAB.EVENT_MANAGEMENT)}
-            />
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.EVENT_MANAGEMENT
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.EVENT_MANAGEMENT}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={
+                      activeTab === TAB.EVENT_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
+                    }
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.EVENT_MANAGEMENT ? true : false}
+                onClick={() =>
+                  getSelectedNavigationOption(TAB.EVENT_MANAGEMENT)
+                }
+              />
 
-            <CustomButton
-              buttonType={
-                activeTab === TAB.USER_MANAGEMENT
-                  ? BUTTONTYPES.WHITE
-                  : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.USER_MANAGEMENT}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={
-                    activeTab === TAB.USER_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
-                  }
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.USER_MANAGEMENT ? true : false}
-              onClick={() => getSelectedNavigationOption(TAB.USER_MANAGEMENT)}
-            />
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.USER_MANAGEMENT
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.USER_MANAGEMENT}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={
+                      activeTab === TAB.USER_MANAGEMENT ? "#5c5dce" : "#a6a6ed"
+                    }
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.USER_MANAGEMENT ? true : false}
+                onClick={() => getSelectedNavigationOption(TAB.USER_MANAGEMENT)}
+              />
 
-            {/* <CustomButton
+              {/* <CustomButton
               buttonType={
                 activeTab === TAB.SETTINGS
                   ? BUTTONTYPES.WHITE
@@ -178,45 +190,43 @@ const SideBar = ({ setCurrentPage }) => {
               onClick={() => getSelectedNavigationOption(TAB.SETTINGS)}
             /> */}
 
-            <CustomButton
-              buttonType={
-                activeTab === TAB.LOGOUT ? BUTTONTYPES.WHITE : BUTTONTYPES.GHOST
-              }
-              buttonText={TAB.LOGOUT}
-              iconsLeft={
-                <DashboardIcon
-                  size={25}
-                  color={activeTab === TAB.LOGOUT ? "#5c5dce" : "#a6a6ed"}
-                />
-              }
-              iconsRight={""}
-              isDisabled={false}
-              active={activeTab === TAB.LOGOUT ? true : false}
-              onClick={() => {
-                getSelectedNavigationOption(TAB.LOGOUT);
-                setIsModalOpen(true);
-              }}
-            />
-          </ul>
+              <CustomButton
+                buttonType={
+                  activeTab === TAB.LOGOUT
+                    ? BUTTONTYPES.WHITE
+                    : BUTTONTYPES.GHOST
+                }
+                buttonText={TAB.LOGOUT}
+                iconsLeft={
+                  <DashboardIcon
+                    size={25}
+                    color={activeTab === TAB.LOGOUT ? "#5c5dce" : "#a6a6ed"}
+                  />
+                }
+                iconsRight={""}
+                isDisabled={false}
+                active={activeTab === TAB.LOGOUT ? true : false}
+                onClick={() => {
+                  getSelectedNavigationOption(TAB.LOGOUT);
+                  setIsModalOpen(true);
+                }}
+              />
+            </ul>
+          </div>
+        </div>
+        <div className={Styles.homeBtn}>
+          <CustomButton
+            buttonType={BUTTONTYPES.SQUAREICON}
+            buttonText={""}
+            iconsLeft={<HomeIcon size={25} color={"#4A49B9"} />}
+            iconsRight={""}
+            isDisabled={false}
+            active={activeTab === TAB.SETTINGS ? true : false}
+            onClick={() => getSelectedNavigationOption(TAB.HOME)}
+          />
         </div>
       </div>
-      <div className={Styles.homeBtn}>
-        <CustomButton
-          buttonType={BUTTONTYPES.SQUAREICON}
-          buttonText={""}
-          iconsLeft={<HomeIcon size={25} color={"#4A49B9"} />}
-          iconsRight={""}
-          isDisabled={false}
-          active={activeTab === TAB.SETTINGS ? true : false}
-          onClick={() => getSelectedNavigationOption(TAB.HOME)}
-        />
-      </div>
-      {isModalOpen ? (
-        <CustomModal open={setIsModalOpen} title={"Logout"} />
-      ) : (
-        ""
-      )}
-    </div>
+    </>
   );
 };
 
