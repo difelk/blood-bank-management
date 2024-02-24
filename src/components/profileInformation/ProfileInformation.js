6e5a5ca087ae245b76a59b24088a2933a30ed5e1
ilmeedesilva <ilmeedesilva@gmail.com>
2024-02-24 19:35:55 +0530

    Profile Info form created with validations - 24/02

--- src/components/profileInformation/ProfileInformation.js
+++ src/components/profileInformation/ProfileInformation.js
@@ -15,8 +15,13 @@ const ProfileInformation = () => {
   const initialValues = {
     first_name: "",
     last_name: "",
-    gender: "",
-    birthday: "",
+    contact_no: "",
+    nic: "",
+    no: "",
+    street: "",
+    city: "",
+    // gender: "",
+    // birthday: "",
   };
 
   const validate = (values) => {
@@ -36,9 +41,41 @@ const ProfileInformation = () => {
       errors.last_name = "Last Name must contain only letters and spaces";
     }
 
-    if (!values.birthday) {
-      errors.birthday = "Birthday is required";
+    if (!values.contact_no) {
+      errors.contact_no = "Contact Number is required";
+    } else if (!/^(0\d{9})$/.test(values.contact_no)) {
+      errors.contact_no = "Invalid Contact Number";
     }
+
+    if (!values.nic) {
+      errors.nic = "NIC is required";
+    } else {
+      if (!/^[0-9]{9}[vVxX]$/.test(values.nic.toUpperCase())) {
+        if (!/^[0-9]{12}$/.test(values.nic)) {
+          errors.nic = "Invalid NIC";
+        }
+      }
+    }
+
+    if (!values.no) {
+      errors.no = "Address No is required";
+    }
+
+    if (!values.street) {
+      errors.street = "Street address is required";
+    } else if (!/^[^\d\s]{2,}/.test(values.street)) {
+      errors.street = "Street address should contain at least 2 letters";
+    }
+
+    if (!values.city) {
+      errors.city = "City is required";
+    } else if (!/^[^\d\s]{3,}/.test(values.city)) {
+      errors.city = "City should contain at least 3 letters";
+    }
+
+    // if (!values.birthday) {
+    //   errors.birthday = "Birthday is required";
+    // }
 
     return errors;
   };
@@ -92,7 +129,94 @@ const ProfileInformation = () => {
                 <span>{errors.last_name}</span>
               </div>
             </div>
+
             <div className={styles.inputWrapper}>
+              <div className={[styles.groupInputs, styles.input50].join(" ")}>
+                <CustomInput
+                  placeHolder={"Contact No"}
+                  id={"contact_no"}
+                  name={"contact_no"}
+                  disabled={false}
+                  getValue={(value) => {
+                    setFieldValue("contact_no", value);
+                  }}
+                  default={initialValues.contact_no ?? ""}
+                  error={errors.contact_no}
+                  type={"text"}
+                  touched={touched}
+                />
+                <span>{errors.contact_no}</span>
+              </div>
+              <div className={[styles.groupInputs, styles.input50].join(" ")}>
+                <CustomInput
+                  placeHolder={"NIC"}
+                  id={"nic"}
+                  name={"nic"}
+                  disabled={false}
+                  getValue={(value) => {
+                    setFieldValue("nic", value);
+                  }}
+                  default={initialValues.nic ?? ""}
+                  error={errors.nic}
+                  type={"text"}
+                  touched={touched}
+                />
+                <span>{errors.nic}</span>
+              </div>
+            </div>
+
+            <div className={styles.inputWrapper}>
+              <div className={[styles.groupInputs, styles.input40].join(" ")}>
+                <CustomInput
+                  placeHolder={"No"}
+                  id={"no"}
+                  name={"no"}
+                  disabled={false}
+                  getValue={(value) => {
+                    setFieldValue("no", value);
+                  }}
+                  default={initialValues.no ?? ""}
+                  error={errors.no}
+                  type={"text"}
+                  touched={touched}
+                />
+                <span>{errors.no}</span>
+              </div>
+              <div className={[styles.groupInputs, styles.input40].join(" ")}>
+                <CustomInput
+                  placeHolder={"Street"}
+                  id={"street"}
+                  name={"street"}
+                  disabled={false}
+                  getValue={(value) => {
+                    setFieldValue("street", value);
+                  }}
+                  default={initialValues.street ?? ""}
+                  error={errors.street}
+                  type={"text"}
+                  touched={touched}
+                />
+                <span>{errors.street}</span>
+              </div>
+              <div className={[styles.groupInputs, styles.input40].join(" ")}>
+                <CustomInput
+                  placeHolder={"City"}
+                  id={"city"}
+                  name={"city"}
+                  disabled={false}
+                  getValue={(value) => {
+                    setFieldValue("city", value);
+                  }}
+                  default={initialValues.city ?? ""}
+                  error={errors.city}
+                  type={"text"}
+                  touched={touched}
+                />
+                <span>{errors.city}</span>
+              </div>
+            </div>
+
+            {/* <div className={styles.inputWrapper}>
               <div className={[styles.groupInputs, styles.input50].join(" ")}>
                 <div className={styles.mgn12}>
                   <CustomDropdown
@@ -118,7 +242,7 @@ const ProfileInformation = () => {
                   <span>{errors.birthday}</span>
                 </div>
               </div>
-            </div>
+            </div> */}
             <div className={styles.submitBtnWrapper}>
               <CustomButton
                 buttonText={"Save"}
