9db28ed228c88224a94931d49384bed411c2cb6d
ilmeedesilva <ilmeedesilva@gmail.com>
2024-02-24 20:18:04 +0530

    Hospital profile info with validations - 24/02

--- src/components/profileInformation/ProfileInformation.js
+++ src/components/profileInformation/ProfileInformation.js
@@ -12,7 +12,9 @@ const sampleDropdownData = [
 ];
 
 const ProfileInformation = () => {
-  const initialValues = {
+  const [isHopitalUser, setIsHospitalUser] = useState(true);
+
+  const normalUserInitialValues = {
     first_name: "",
     last_name: "",
     contact_no: "",
@@ -24,7 +26,18 @@ const ProfileInformation = () => {
     // birthday: "",
   };
 
-  const validate = (values) => {
+  const hospitalUserInitialValues = {
+    contact_no: "",
+    no: "",
+    street: "",
+    city: "",
+    hospital_location: "",
+    hospital_name: "",
+    // gender: "",
+    // birthday: "",
+  };
+
+  const validateNormalUser = (values) => {
     const errors = {};
 
     console.log("values - ", values);
@@ -80,6 +93,48 @@ const ProfileInformation = () => {
     return errors;
   };
 
+  const validateHospitalUser = (values) => {
+    const errors = {};
+
+    console.log("values - ", values);
+
+    if (!values.contact_no) {
+      errors.contact_no = "Contact Number is required";
+    } else if (!/^(0\d{9})$/.test(values.contact_no)) {
+      errors.contact_no = "Invalid Contact Number";
+    }
+
+    if (!values.no) {
+      errors.no = "Address No is required";
+    }
+
+    if (!values.street) {
+      errors.street = "Street address is required";
+    } else if (!/^[^\d\s]{2,}/.test(values.street)) {
+      errors.street = "Street address should contain at least 2 letters";
+    }
+
+    if (!values.city) {
+      errors.city = "City is required";
+    } else if (!/^[^\d\s]{3,}/.test(values.city)) {
+      errors.city = "City should contain at least 3 letters";
+    }
+
+    if (!values.hospital_name) {
+      errors.hospital_name = "Hospital Name is required";
+    } else if (!/^[^\d\s]{2,}/.test(values.hospital_name)) {
+      errors.hospital_name = "Hospital Name should contain at least 2 letters";
+    }
+
+    if (!values.hospital_location) {
+      errors.hospital_location = "Main Location is required";
+    } else if (!/^[^\d\s]{3,}/.test(values.hospital_location)) {
+      errors.hospital_location = "Main Location contain at least 3 letters";
+    }
+
+    return errors;
+  };
+
   const handleSubmit = (values) => {
     console.log("values - ", values);
     setTimeout(() => {}, 400);
@@ -88,46 +143,75 @@ const ProfileInformation = () => {
   return (
     <div>
       <Formik
-        initialValues={initialValues}
-        validate={validate}
+        initialValues={
+          isHopitalUser ? hospitalUserInitialValues : normalUserInitialValues
+        }
+        validate={isHopitalUser ? validateHospitalUser : validateNormalUser}
         onSubmit={handleSubmit}
         validateOnBlur
       >
         {({ isSubmitting, values, errors, touched, setFieldValue }) => (
           <Form>
             <div className={styles.inputWrapper}>
-              <div className={[styles.groupInputs, styles.input50].join(" ")}>
-                <CustomInput
-                  placeHolder={"First Name"}
-                  id={"first_name"}
-                  name={"first_name"}
-                  disabled={false}
-                  getValue={(value) => {
-                    setFieldValue("first_name", value);
-                  }}
-                  default={initialValues.first_name ?? ""}
-                  error={errors.first_name}
-                  type={"text"}
-                  touched={touched}
-                />
-                <span>{errors.first_name}</span>
-              </div>
-              <div className={[styles.groupInputs, styles.input50].join(" ")}>
-                <CustomInput
-                  placeHolder={"Last Name"}
-                  id={"last_name"}
-                  name={"last_name"}
-                  disabled={false}
-                  getValue={(value) => {
-                    setFieldValue("last_name", value);
-                  }}
-                  default={initialValues.last_name ?? ""}
-                  error={errors.last_name}
-                  type={"text"}
-                  touched={touched}
-                />
-                <span>{errors.last_name}</span>
-              </div>
+              {!isHopitalUser ? (
+                <>
+                  <div
+                    className={[styles.groupInputs, styles.input50].join(" ")}
+                  >
+                    <CustomInput
+                      placeHolder={"First Name"}
+                      id={"first_name"}
+                      name={"first_name"}
+                      disabled={false}
+                      getValue={(value) => {
+                        setFieldValue("first_name", value);
+                      }}
+                      default={values.first_name ?? ""}
+                      error={errors.first_name}
+                      type={"text"}
+                      touched={touched}
+                    />
+                    <span>{errors.first_name}</span>
+                  </div>
+                  <div
+                    className={[styles.groupInputs, styles.input50].join(" ")}
+                  >
+                    <CustomInput
+                      placeHolder={"Last Name"}
+                      id={"last_name"}
+                      name={"last_name"}
+                      disabled={false}
+                      getValue={(value) => {
+                        setFieldValue("last_name", value);
+                      }}
+                      default={values.last_name ?? ""}
+                      error={errors.last_name}
+                      type={"text"}
+                      touched={touched}
+                    />
+                    <span>{errors.last_name}</span>
+                  </div>
+                </>
+              ) : (
+                <div
+                  className={[styles.groupInputs, styles.input100].join(" ")}
+                >
+                  <CustomInput
+                    placeHolder={"Hospital Name"}
+                    id={"hospital_name"}
+                    name={"hospital_name"}
+                    disabled={false}
+                    getValue={(value) => {
+                      setFieldValue("hospital_name", value);
+                    }}
+                    default={values.hospital_name ?? ""}
+                    error={errors.hospital_name}
+                    type={"text"}
+                    touched={touched}
+                  />
+                  <span>{errors.hospital_name}</span>
+                </div>
+              )}
             </div>
 
             <div className={styles.inputWrapper}>
@@ -140,33 +224,56 @@ const ProfileInformation = () => {
                   getValue={(value) => {
                     setFieldValue("contact_no", value);
                   }}
-                  default={initialValues.contact_no ?? ""}
+                  default={values.contact_no ?? ""}
                   error={errors.contact_no}
                   type={"text"}
                   touched={touched}
                 />
                 <span>{errors.contact_no}</span>
               </div>
-              <div className={[styles.groupInputs, styles.input50].join(" ")}>
-                <CustomInput
-                  placeHolder={"NIC"}
-                  id={"nic"}
-                  name={"nic"}
-                  disabled={false}
-                  getValue={(value) => {
-                    setFieldValue("nic", value);
-                  }}
-                  default={initialValues.nic ?? ""}
-                  error={errors.nic}
-                  type={"text"}
-                  touched={touched}
-                />
-                <span>{errors.nic}</span>
-              </div>
+              {!isHopitalUser ? (
+                <>
+                  <div
+                    className={[styles.groupInputs, styles.input50].join(" ")}
+                  >
+                    <CustomInput
+                      placeHolder={"NIC"}
+                      id={"nic"}
+                      name={"nic"}
+                      disabled={false}
+                      getValue={(value) => {
+                        setFieldValue("nic", value);
+                      }}
+                      default={values.nic ?? ""}
+                      error={errors.nic}
+                      type={"text"}
+                      touched={touched}
+                    />
+                    <span>{errors.nic}</span>
+                  </div>
+                </>
+              ) : (
+                <div className={[styles.groupInputs, styles.input50].join(" ")}>
+                  <CustomInput
+                    placeHolder={"Main Location"}
+                    id={"hospital_location"}
+                    name={"hospital_location"}
+                    disabled={false}
+                    getValue={(value) => {
+                      setFieldValue("hospital_location", value);
+                    }}
+                    default={values.hospital_location ?? ""}
+                    error={errors.hospital_location}
+                    type={"text"}
+                    touched={touched}
+                  />
+                  <span>{errors.hospital_location}</span>
+                </div>
+              )}
             </div>
 
             <div className={styles.inputWrapper}>
-              <div className={[styles.groupInputs, styles.input40].join(" ")}>
+              <div className={[styles.groupInputs, styles.input30].join(" ")}>
                 <CustomInput
                   placeHolder={"No"}
                   id={"no"}
@@ -175,14 +282,14 @@ const ProfileInformation = () => {
                   getValue={(value) => {
                     setFieldValue("no", value);
                   }}
-                  default={initialValues.no ?? ""}
+                  default={values.no ?? ""}
                   error={errors.no}
                   type={"text"}
                   touched={touched}
                 />
                 <span>{errors.no}</span>
               </div>
-              <div className={[styles.groupInputs, styles.input40].join(" ")}>
+              <div className={[styles.groupInputs, styles.input30].join(" ")}>
                 <CustomInput
                   placeHolder={"Street"}
                   id={"street"}
@@ -191,14 +298,14 @@ const ProfileInformation = () => {
                   getValue={(value) => {
                     setFieldValue("street", value);
                   }}
-                  default={initialValues.street ?? ""}
+                  default={values.street ?? ""}
                   error={errors.street}
                   type={"text"}
                   touched={touched}
                 />
                 <span>{errors.street}</span>
               </div>
-              <div className={[styles.groupInputs, styles.input40].join(" ")}>
+              <div className={[styles.groupInputs, styles.input30].join(" ")}>
                 <CustomInput
                   placeHolder={"City"}
                   id={"city"}
@@ -207,7 +314,7 @@ const ProfileInformation = () => {
                   getValue={(value) => {
                     setFieldValue("city", value);
                   }}
-                  default={initialValues.city ?? ""}
+                  default={values.city ?? ""}
                   error={errors.city}
                   type={"text"}
                   touched={touched}
