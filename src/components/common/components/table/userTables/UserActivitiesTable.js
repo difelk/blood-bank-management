d0547ad1086b15f1173018f3e5f4636bc77853d6
difelk <dilshan.f@theavo.net>
2024-02-28 07:03:09 +0530

    created user activity log tables

---
+++ src/components/common/components/table/userTables/UserActivitiesTable.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import styles from "./UserTable.module.scss";
+import CustomButton from "../../customButton";
+import ViewMoreIcon from "../../../../../assets/icons/svgs/ViewMore";
+import CustomModal from "../../modal/CustomModal";
+import UserActivtiesListTable from "./UserActivtiesListTable";
+
+const tableHeader = [
+  { name: "Activity Name", width: "25%" },
+  { name: "Date", width: "25%" },
+  { name: "Description", width: "50%" },
+];
+
+const UserActivitiesTable = ({
+  tableHeader,
+  dataset,
+  actionType,
+  isAllowedFullAccess,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState({});
+
+  const ScrollToTopButton = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    ScrollToTopButton();
+  }, [isModalOpen]);
+
+  return (
+    <div className={styles.tableWrapper}>
+      <div className={styles.tableHeader}>
+        {tableHeader.map((header) => (
+          <div
+            className={styles.tableHeaderItem}
+            style={{ width: header.width }}
+          >
+            <p>{header.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className={styles.tableBody}>
+        {dataset.map((item, index) => (
+          <div className={styles.tableData}>
+            <div
+              className={styles.tableDataItem}
+              style={{ width: tableHeader[0].width }}
+            >
+              <p>{item.nic}</p>
+            </div>
+            <div
+              className={styles.tableDataItem}
+              style={{ width: tableHeader[1].width }}
+            >
+              <p>{item.username}</p>
+            </div>
+            <div
+              className={styles.tableDataItem}
+              style={{ width: tableHeader[2].width }}
+            >
+              <p>{item.role}</p>
+            </div>
+            <div
+              className={styles.tableDataItem}
+              style={{ width: tableHeader[3].width }}
+            >
+              <p>{item.organization}</p>
+            </div>
+            <div
+              className={styles.tableDataItem}
+              style={{ width: tableHeader[4].width }}
+            >
+              <CustomButton
+                buttonType={"ICON"}
+                iconsLeft={<ViewMoreIcon size={18} color={"#BBB6B4"} />}
+                onClick={() => {
+                  setIsModalOpen(true);
+                  setSelectedUser(item);
+                }}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      {isModalOpen ? (
+        <CustomModal
+          open={setIsModalOpen}
+          title={`User ${selectedUser.nic} Activities`}
+        >
+          <div className={styles.hospitalData}>
+            <UserActivtiesListTable
+              dataset={[]}
+              tableHeader={tableHeader}
+              actionType={"VIEW"}
+              isAllowedFullAccess={true}
+            />
+          </div>
+        </CustomModal>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+export default UserActivitiesTable;
