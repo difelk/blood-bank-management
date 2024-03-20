243ec4e969648c9554821ff94f74708e504585c0
difelk <dilshan.f@theavo.net>
2024-03-20 22:53:58 +0530

    fixed a bug in user form, added all the types into a new file

---
+++ src/share/DataType.js
@@ -0,0 +1,32 @@
+export const BLOODGROUP = [
+  { key: "A+", value: "A +" },
+  { key: "A-", value: "A -" },
+  { key: "B+", value: "B +" },
+  { key: "B-", value: "B -" },
+  { key: "AB+", value: "AB +" },
+  { key: "AB-", value: "AB -" },
+  { key: "O+", value: "O +" },
+  { key: "O-", value: "O -" },
+];
+
+export const userGroups = [
+  { key: "user", value: "User" },
+  { key: "admin", value: "Admin" },
+  { key: "superAdmin", value: "Super Admin" },
+];
+
+export const organizationTypes = [
+  { key: "bloodBank", value: "Blood Bank" },
+  { key: "hospital", value: "Hospital" },
+];
+
+export const organizations = [
+  { key: "hospital1", value: "Hospital 01" },
+  { key: "hospital2", value: "Hospital 02" },
+  { key: "hospital3", value: "Hospital 03" },
+];
+
+export const gender = [
+  { key: "male", value: "Male" },
+  { key: "female", value: "Female" },
+];
